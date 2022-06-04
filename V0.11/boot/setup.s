d8097138bb4a4a10420b999c372afb159d618b91
yanyinuo <1601530253@qq.com>
2022-06-04 14:46:02 +0800

    6-4

--- V0.11/boot/setup.s
+++ V0.11/boot/setup.s
@@ -1,6 +1,3 @@
-!
-!	setup.s		(C) 1991 Linus Torvalds
-!
 ! setup.s is responsible for getting the system data from the BIOS,
 ! and putting them into the appropriate places in system memory.
 ! both setup.s and system has been loaded by the bootblock.
@@ -44,7 +41,7 @@ start:
 	*/
 	mov	[0],dx		! it from 0x90000.
 	/*
-	 int 0x10 中断程序执行完毕并返回时，dx 寄存器里的值表示光标的位置，高八位 dh 存储了行号，低八位 dl 存储了列号.
+	 int 0x10 中断程序执行完毕并返回时,dx 寄存器里的值表示光标的位置，高八位 dh 存储了行号，低八位 dl 存储了列号.
 	 mov [0],dx 就是把这个光标位置存储在 [0] 这个内存地址处。注意，这个内存地址仅仅是偏移地址，还需要加上 ds 这个寄存器里存储的段基址，最终的内存地址是在 0x90000 处，这里存放着光标的位置，以便之后在初始化控制台的时候用到.
 	*/
 ! Get memory size (extended mem, kB) 获取内存信息
@@ -111,9 +108,8 @@ no_disk1:
 	stosb
 is_disk1:
 
-! now we want to move to protected mode ...
-
-	cli			!关闭中断
+! now we want to move to protected mode ... 即关闭中断
+	cli			
 
 ! first we move the system to it's rightful place
 
@@ -139,9 +135,8 @@ end_move:
 	mov	ax,#SETUPSEG	! right, forgot this at first. didn't work :-)
 	mov	ds,ax
 	lidt	idt_48		! load idt with 0,0
-	lgdt	gdt_48		! load gdt with whatever appropriate
-
-! that was painless, now we enable A20
+	lgdt	gdt_48		
+	!把后面的值（gdt_48）放在 gdtr 寄存器中，gdt_48 标签
 
 	call	empty_8042
 	mov	al,#0xD1		! command write
@@ -150,14 +145,6 @@ end_move:
 	mov	al,#0xDF		! A20 on
 	out	#0x60,al
 	call	empty_8042
-
-! well, that went ok, I hope. Now we have to reprogram the interrupts :-(
-! we put them right after the intel-reserved hardware interrupts, at
-! int 0x20-0x2F. There they won't mess up anything. Sadly IBM really
-! messed this up with the original PC, and they haven't been able to
-! rectify it afterwards. Thus the bios puts interrupts at 0x08-0x0f,
-! which is used for the internal hardware interrupts as well. We just
-! have to reprogram the 8259's, and it isn't fun.
 
 	mov	al,#0x11		! initialization sequence
 	out	#0x20,al		! send it to 8259A-1
@@ -186,23 +173,11 @@ end_move:
 	.word	0x00eb,0x00eb
 	out	#0xA1,al
 
-! well, that certainly wasn't fun :-(. Hopefully it works, and we don't
-! need no steenking BIOS anyway (except for the initial loading :-).
-! The BIOS-routine wants lots of unnecessary data, and it's less
-! "interesting" anyway. This is how REAL programmers do it.
-!
-! Well, now's the time to actually move into protected mode. To make
-! things as simple as possible, we do no register set-up or anything,
-! we let the gnu-compiled 32-bit programs do that. We just jump to
-! absolute address 0x00000, in 32-bit protected mode.
 
 	mov	ax,#0x0001	! protected mode (PE) bit
 	lmsw	ax		! This is it!
 	jmpi	0,8		! jmp offset 0 of segment 8 (cs)
 
-! This routine checks that the keyboard command queue is empty
-! No timeout is used - if this hangs there is something wrong with
-! the machine, and we probably couldn't proceed anyway.
 empty_8042:
 	.word	0x00eb,0x00eb
 	in	al,#0x64	! 8042 status port
@@ -210,7 +185,7 @@ empty_8042:
 	jnz	empty_8042	! yes - loop
 	ret
 
-gdt:
+gdt:  !gdt标签处，就是全局描述符表在内存中的真正数据
 	.word	0,0,0,0		! dummy
 
 	.word	0x07FF		! 8Mb - limit=2047 (2048*4096=8Mb)
@@ -226,11 +201,14 @@ gdt:
 idt_48:
 	.word	0			! idt limit=0
 	.word	0,0			! idt base=0L
+//中断描述符表是用来在发生中断时，CPU 拿着中断号去中断描述符表中寻找中断处理程序的地址，找到后就跳到相应的中断程序中去执行
 
-gdt_48:
-	.word	0x800		! gdt limit=2048, 256 GDT entries
-	.word	512+gdt,0x9	! gdt base = 0X9xxxx
-	
+gdt_48:    !表示一个 48 位的数据，其中高 32 位存储着的正是全局描述符表 gdt 的内存地址
+	.word	0x800		 ! gdt limit=2048, 256 GDT entries
+	.word	512+gdt,0x9	 !gdt 是个标签，表示在本文件内的偏移量，而本文件是 setup.s，编译后是放在 0x90200 这个内存地址的，所以要加上 0x90200 这个值。
+
+
+
 .text
 endtext:
 .data
